644e669c93bfafd112260d888841c07fd6f2784d
Brian Clark <brclar15@gmail.com>
2018-05-24 14:35:27 -0400

    Update compareForms.py

--- code/compareForms.py
+++ code/compareForms.py
@@ -1,82 +1,89 @@
 # import Element Tree XML Parser
 import xml.etree.ElementTree as etree
 
-fileName1 = '/Users/brian/Desktop/vergil.xml'
-fileName2 = '/Users/brian/Desktop/caesar1.xml'
 
-tree1 = etree.parse(fileName1)
-root1 = tree1.getroot()
+# intial start up print statements
+print("This program allows you to find the different words used in a corpus for certain relations.")
+print("When choosing a file, please enter the file name in a format that mirrors this: /Users/brian/Desktop/ovid13.xml \n")
+print("HINT: This script needs to be in the same folder as your .xml file in order for it to work, and the file path must be typed correctly. \n")
+fileName = input("Please enter a treebank XML file: ")
 
-tree2 = etree.parse(fileName2)
-root2 = tree2.getroot()
 
-author1 = fileName1.split('/')                # Gets the author//book in a readable format
-author1 = author1[-1][:-4]                    # to print later
+tree = etree.parse(fileName)
+root = tree.getroot()
 
-author2 = fileName2.split('/')     
-author2 = author2[-1][:-4]  
 
-def countWords(root):
-    count = 0
-    for element in root.getiterator('word'):
-        if element.get('lemma') != "punc1":
-            if element.get('artificial') != "elliptic":   # skips over words added to fillin ellipsis
-                count += 1
-    return count
-
+def authorInfo(fileName):
+    author = fileName.split('/')                # Gets the author//book in a readable format
+    author = author[-1][:-4]                    # to print later
+    print("Currently analyzing :", author, "\n")
+    
+    
 
 
 # Compare two trees for a specific tag, gives back form 
-def findTag(relation, root1, root2):
-    tagList1 = list()
-    tagList2 = list()
-    tagDict1 = {}
-    tagDict2 = {}
-    for element in root1.getiterator('word'):
+def findTag(relation):
+    tagList = list()
+    tagDict = {}
+    for element in root.getiterator('word'):
         if element.get('relation') == relation:
             if element.get('artificial') != "elliptic": 
-                tagList1.append(element.get('lemma'))
-    for element in root2.getiterator('word'):
-        if element.get('relation') == relation:
-            if element.get('artificial') != "elliptic": 
-                tagList2.append(element.get('lemma'))
-    tagList1.sort()
-    tagList2.sort()
-    total1 = len(tagList1)
-    total2 = len(tagList2)
+                tagList.append(element.get('lemma'))
+    tagList.sort()
+    total = len(tagList)
 
-    for item in tagList1:
-        tagDict1[item] = tagList1.count(item)
-    for item in tagList2:
-        tagDict2[item] = tagList2.count(item)
-        
-    print(author1, "has", total1, relation, "\n")
-    print(tagDict1)
-    print("\n", author2, "has", total2, relation, "\n")
-    print(tagDict2)
+    for item in tagList:
+        tagDict[item] = tagList.count(item)
+    print("Here are the raw numbers" )
+    print("Total number of relations for ", relation, ":", total, "\n")
+
+    for key, value in tagDict.items():
+        print(key, ",", value, sep="")
+    print("\n")
     
-               
-
-
-findTag("COORD", root1, root2)
-
 
 
 # Gets all of the possible different relation tags and adds them to a list
 TBtagSet = set()
-for element in root1.getiterator('word'):
+for element in root.getiterator('word'):
     TBtagSet.add(element.get('relation'))
 TBtagSet2 = set()
-for element in root2.getiterator('word'):
-    TBtagSet2.add(element.get('relation'))
-newSet = TBtagSet2 - TBtagSet
-for item in newSet:
-    TBtagSet.add(item)
 
 # Make the set a list, and sort it
 TBtagList = list(TBtagSet)
 TBtagList.sort()
 
+
+############# User interface starts here
+
+def startUp():
+    allowedList = ["COORD","AuxP","AuxC"]
+    while True:    
+        print("\n Please enter COORD for coordinators, AuxP for preopostions, or AuxC for subordinating conjunctions. \n")               
+        userChoice = input("Which of these three choices are you interested in? : ")
+        if userChoice not in allowedList:
+            print("ERROR: you must choose from the options above.")
+            break
+        else:
+            authorInfo(fileName)
+            findTag(userChoice)
+            break
+
+startUp()
+
+while True:
+    print("would you like to run the program again on a different relation? \n")
+    newInput = input("Enter yes or no: ")
+    if newInput != "yes" and newInput != "no":
+        print("ERROR: you must choose 'yes' or 'no'. Please rerun the program.")
+        break
+    elif newInput == "yes":
+        startUp()
+    elif newInput == "no":
+        print("Alright, goodbye!")
+        break 
+    
+    
 
 
     
